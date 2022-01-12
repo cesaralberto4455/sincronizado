0db52c23613ea252a94ff33d1717a3d60fe7f091
Cesar <cesaralberto4455@gmail.com>
2022-01-12 00:01:38 -0500

    modificando localmente el archivo nav.php

---
+++ nav.php
@@ -0,0 +1,8 @@
+<header>
+    <ol>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Home</a></li>
+    </ol>
+</header>
